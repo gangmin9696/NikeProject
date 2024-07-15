5a1d5ca7fbab51409e5f568bf8048775b802a085
gangmin9696 <poi2308@naver.com>
2024-07-15 13:12:24 +0900

    test

--- src/views/CartView.jsx
+++ src/views/CartView.jsx
@@ -1,11 +1,7 @@
-import React from 'react';
+import React from "react";
 
 const CartView = () => {
-  return (
-    <div>
-      장바구니 페이지 - 코드 변경 테스트
-    </div>
-  );
+    return <div>장바구니 페이지</div>;
 };
 
 export default CartView;
